d81804feab9f70017a2badf464e25ffd581ce00d
Tumenjargal Altanginj <tumenjargal@pink.kiwi>
2024-10-16 07:22:13 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+interface GitHubUser {
+  login: string;
+  avatar_url: string;
+  html_url: string;
+}
+
 function App() {
+  const [query, setQuery] = useState<string>('');
+  const [users, setUsers] = useState<GitHubUser[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+
+  const fetchGitHubUsers = async (searchQuery: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(`https://api.github.com/search/users?q=${searchQuery}`);
+      setUsers(response.data.items);
+    } catch (err) {
+      setError('Error fetching users');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = () => {
+    if (query.trim()) {
+      fetchGitHubUsers(query);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div className="container">
+        <section className="jumbotron">
+          <h3 className="jumbotron-heading">Search Github Users</h3>
+          <div>
+            <input value={query} onChange={e => setQuery(e.target.value)} type="text" placeholder="enter the name you search" />&nbsp;<button onClick={handleSearch}>Search</button>
+          </div>
+        </section>
+
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        <div className="row">
+          {
+            users.map((u) => <div className="card">
+              <a href={u.html_url} target="_blank">
+                <img src={u.avatar_url} style={{ width: "100px" }} />
+              </a>
+              <p className="card-text">{u.login}</p>
+            </div>)
+          }
+        </div>
+      </div>
     </div>
   );
 }
